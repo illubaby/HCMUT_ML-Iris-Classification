ce84099184f9ec173717c3a0541826ada955ab93
illubaby <aironb177@gmail.com>
2024-01-03 19:26:46 +0700

    KNN

---
+++ Model/KNN_Classifier.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pd.read_csv(url, names=names)
+
+# Data Visualization
+sns.pairplot(dataset, hue='class')
+plt.show()
+
+# Data Preprocessing
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Model Selection
+model = KNeighborsClassifier(n_neighbors=5)
+
+# Model Training and Evaluation
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+
+print(confusion_matrix(y_test, predictions))
+print(classification_report(y_test, predictions))
